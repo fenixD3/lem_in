bdb9d96b87b9262798922589794d1209782099cc
mdeanne <mdeanne@oa-d4.21-school.ru>
2020-01-16 00:03:46 +0300

    wave-recursive re-wrote

--- ways/finding.c
+++ ways/finding.c
@@ -5,35 +5,89 @@
 #include "../includes/lem_in.h"
 
 #include <stdio.h>
+#include <stdbool.h>
 
-_Bool	set_wave(t_room *room, t_room *end, int prev_depth, t_grp *grp)
+
+
+
+int		give_lenlink(t_room *room, t_grp *grp)
+{
+	t_link	*link;
+	int		len;
+
+	link = room->link;
+	len = 0;
+	while (link)
+	{
+		if (link->room != grp->start)
+		len++;
+		link = link->next;
+	}
+	return (len);
+}
+
+t_link	*can_set_depth(t_room *room)
+{
+	t_link	*link;
+
+	link = room->link;
+	while (link)
+	{
+		if ((link->room->depth != 0 && link->room->depth > room->depth))
+			return (link);
+		link = link->next;
+	}
+	return (link);
+}
+
+int		set_depth(t_room *room, t_grp *grp)
+{
+	t_link	*link;
+	int 	setted;
+
+	link = room->link;
+	setted = 0;
+	while (link)
+	{
+		if ((link->room->depth == 0 && grp->start != link->room) ||
+			link->room->depth > room->depth + 1)
+		{
+			link->room->depth = room->depth + 1;
+			setted++;
+		}
+		link = link->next;
+	}
+	return (setted);
+}
+
+int 	set_wave(t_room *room, int depth, t_grp *grp)
 {
 	t_link *link;
 
-	link = room->link;
-	prev_depth++;
+	if (set_depth(room, grp) || depth <= room->depth)
+		return (0);
+
 	while (link)
 	{
-		printf("%s\n", link->room->name);
-		print_rooms_with_depth(grp->room);
-		printf("--------------\n");
-		if (end == link->room && link->room->depth != 0)
-			return (1);
-		if (link->room == grp->start)
-			;
-		else if (link->room->depth == 0 || link->room->depth > prev_depth)
-		{
-			link->room->depth = prev_depth;
-			if (end == link->room)
-				return (1);
-		}
-		else
-			if(set_wave(link->room, end, prev_depth, grp))
-				return (1);
-
+		set_wave(link->room, depth, grp);
 		link = link->next;
 	}
-	return (0);
+}
+
+/*
+** moon causes waves
+*/
+
+void	moon(t_grp *grp)
+{
+	int depth;
+
+	depth = 0;
+	while (grp->end->depth == 0)
+	{
+		set_wave(grp->start, depth, grp);
+		depth++;
+	}
 }
 
 void 	finding_ways(t_grp *grp)
@@ -43,7 +97,48 @@ void 	finding_ways(t_grp *grp)
 
 	room = grp->start;
 	room->depth = 0;
-	while (grp->end->depth == 0)
-		set_wave(grp->start, grp->end, 0, grp);
+	moon(grp);
 	print_rooms_with_depth(grp->room);
 }
+
+
+
+/*_Bool set_wave(t_room *room, int prev_depth, t_grp *grp, _Bool force_go)
+{
+	t_link	*link;
+	int		flag;
+	int		len;
+
+	link = room->link;
+	prev_depth++;
+	len = give_lenlink(room, grp);
+	if ((flag = can_set_depth(room, len, grp)) == -1)
+		return (0);
+	while (link)
+	{
+		printf("\t\t%s %s\n", room->name, link->room->name);
+		printf("--------------\n");
+		print_rooms_with_depth(grp->room);
+
+		if (link->room == grp->start)
+			;
+		else if (flag >= len && set_wave(link->room, prev_depth, grp, 0))
+			return (1);
+		else if (link->room->depth == 0 || link->room->depth > prev_depth)
+		{
+			link->room->depth = prev_depth;
+			if (grp->end == link->room)
+				return (1);
+		}
+		else if (link->room->depth <= prev_depth)
+			flag++;
+		link = link->next;
+		if (!link && len == flag && flag++)
+			link = room->link;
+	}
+	printf("==============\n");
+	printf("\t\t%s\n", room->name);
+	print_rooms_with_depth(grp->room);
+	printf("==============\n");
+	return (0);
+}*/
